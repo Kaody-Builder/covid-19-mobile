281e3806e250960e5aa02050a7fd27920fe85f81
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2020-04-08 19:04:16 +0300

    Acceuil et Confirmation Page Satic - Acceuil - Page de confirmation - Static Front

---
+++ lib/Pages/confirmation.dart
@@ -0,0 +1,192 @@
+import 'package:flutter/material.dart';
+
+
+final colorP = Colors.teal; 
+
+class ConfirmationPage extends StatefulWidget{
+
+  final String numero;
+
+  ConfirmationPage({Key key, this.numero}) : super(key: key);
+
+  @override 
+  _ConfirmationPage createState() => _ConfirmationPage();
+}
+
+
+class _ConfirmationPage extends State<ConfirmationPage>{
+
+  int nbrMs = 0;
+  String nombreMessage = "";
+  @override 
+  Widget build(BuildContext context){
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Page de Confirmation"),
+      ),
+      body: Center(
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.6,
+          width: MediaQuery.of(context).size.width  * (11/12),
+          child: Card(
+            elevation: 3,
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10),
+                child: Text(
+                  "Confirmation d'identité",
+                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black54, fontSize: 26)
+                ),
+              ),
+              SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  "Un $nombreMessage message contenant un code de confirmation a été envoyer au numéro:",
+                  style: TextStyle(color: Colors.black54),  
+                )
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                child: Text(
+                  widget.numero,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height/20,
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: MediaQuery.of(context).size.width/12),
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //     color: Colors.grey,
+                //     width: 1,
+                //   ),
+                //   borderRadius: BorderRadius.circular(12),
+              
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: "Saisissez le code de confirmation",
+                      //border: InputBorder.none
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.symmetric(vertical:5),
+                width: MediaQuery.of(context).size.width*(5/6),
+                // decoration: BoxDecoration(
+                //   color: colorP,
+                //   border: Border.all(color: colorP, width: 1),
+                //   borderRadius: BorderRadius.circular(6)
+                // ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width*(5/18),
+                      // decoration: BoxDecoration(
+                      //   color: Colors.white,
+                      //   border: Border.all(width: 1),
+                      //   borderRadius: BorderRadius.circular(6)
+                      // ),
+                      margin: EdgeInsets.only(right: 15),
+                      
+                      child: FlatButton(
+                        color: Colors.black12,
+                          onPressed: (){
+                            Navigator.of(context).pop();
+                          },
+                          child: Text("Annuler", style: TextStyle(color: Colors.black54)),
+                      )
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width*(5/18),
+                      // decoration: BoxDecoration(
+                      //   color: colorP,
+                      //   border: Border.all(color: colorP, width: 1),
+                      //   borderRadius: BorderRadius.circular(6)
+                      // ),
+                      margin: EdgeInsets.only(left: 15),
+                      child: FlatButton(
+                        color: colorP,
+                        onPressed: (){},
+                        child: Text("Continuer", style: TextStyle(color: Colors.white)),
+                      )
+                    )
+                  ],
+                )
+                
+              ),
+               Container(
+                 margin: EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.center,
+                child: FlatButton(
+                  color: Colors.white,
+                  onPressed: (){
+                    nbrMs++;
+                    _codenonreceive();
+                  },
+                  child: Text(
+                    "Je n'ai réçu pas le code.",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: colorP),
+                  )
+                )
+              )
+            ],
+          ),
+          )
+        )
+      )
+    );
+
+  }
+
+  void nombreMessageChange(){
+    if (nbrMs > 1) nombreMessage = nbrMs.toString() + "éme";
+    else nombreMessage = "";
+  }
+
+  Future<void> _codenonreceive() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          
+          title: Text('Code de cofirmation', style: TextStyle(color: Color(0xFF464637))),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text("Nous vous prions d'attendre quelque minutes.\n"),
+                Text("Vous pouvez renvoyer le code.")
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Renvoyer'),
+              onPressed: () {
+                setState(() {
+                  nbrMs++;
+                  nombreMessageChange();
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,21 @@
+import 'package:covid19_tracker/Pages/confirmation.dart';
 import 'package:flutter/material.dart';
+import 'Pages/confirmation.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final colorP = Colors.teal;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Covid-19 Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: colorP,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Covid-19 Tracker'),
     );
   }
 }
@@ -28,14 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +31,196 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+ 
+  final colorP = Colors.teal;
+  final numeroChamp = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        centerTitle: true,
         title: Text(widget.title),
+        
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Image.asset("assets/logo.png"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                "COVID-19 Tracker",
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black54),
+                textScaleFactor: 2,
+              ),
             ),
+            Container(
+              margin: EdgeInsets.only(top:5),
+              child: Text(
+                "Solution signalement de cas de Corona Virus à Madagascar",
+                style: TextStyle(color: Colors.black54, fontSize: 10)
+              ),
+            )
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: MediaQuery.of(context).size.height * 0.25,
+          // decoration: BoxDecoration(
+          //   border: Border.all(
+          //     color: colorP,
+          //     width: 1,
+          //   ),
+          //   borderRadius: BorderRadius.circular(12),
+          // ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 5),
+                child: Text(
+                  "Entrer votre numéro de téléphone pour confirmer \nvotre implication dans vos signalements:",
+                  maxLines: 2,
+                  style: TextStyle(color: Colors.black54, fontSize: 13.5),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height/22,
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: MediaQuery.of(context).size.width/12),
+                // decoration: BoxDecoration(
+                //   border: Border.all(
+                //     color: Colors.grey,
+                //     width: 1,
+                //   ),
+                //   borderRadius: BorderRadius.circular(12),
+                // ),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  controller: numeroChamp,
+                  decoration: InputDecoration(
+                    hintText: "Votre numéro de téléphone.",
+                      //border: InputBorder.none
+                  ),
+                  keyboardType: TextInputType.text,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical:5),
+                width: MediaQuery.of(context).size.width*(5/6),
+                decoration: BoxDecoration(
+                  color: colorP,
+                  border: Border.all(color: colorP, width: 1),
+                  borderRadius: BorderRadius.circular(6)
+                ),
+                child: FlatButton(
+                  color: colorP,
+                  onPressed: (){
+                    traitement(numeroChamp.text);
+                  },
+                  child: Text("Signaler un cas", style: TextStyle(color: Colors.white)),
+                ),
+              )
+
+            ],
+          ),
+        ),
+      ),
+
+     
     );
   }
+
+  void traitement(String numeroGet){
+    bool validate = false; 
+
+    if (numeroGet.length == 10){
+      RegExp regExp = new RegExp(r"^(03)[2349]{1}[0-9]{7}$");
+      if (regExp.hasMatch(numeroGet)) validate = true;
+    }
+    else if (numeroGet.length == 13){
+      RegExp regExp = new RegExp(r"^(.+2613)[2349]{1}[0-9]{7}$");
+      if (regExp.hasMatch(numeroGet)) validate = true;
+    }
+
+    numeroGet = formNumero(numeroGet);
+    if (validate){
+      Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context){
+          return ConfirmationPage(numero: numeroGet);
+        }));
+    } 
+    else _alertErrorInput();
+    
+  }
+
+  Future<void> _alertErrorInput() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          
+          title: Text('Numéro incorrecte', style: TextStyle(color: Color(0xFFe74c3c))),
+          
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Fermer'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String formNumero(String num){
+    String numFinal = "";
+
+    if (num.length == 13){
+      List<String> numList = num.split('');
+      int i = 0;
+      
+      for (String l in numList){
+        numFinal += l;
+        if (i == 3) numFinal += " ";
+        else if (i == 5) numFinal += " ";
+        else if (i == 7) numFinal += " ";
+        else if (i == 10) numFinal += " ";
+        i++;
+      }
+    }
+
+    else if (num.length == 10){
+      List<String> numList = num.split('');
+      int i = 0;
+      
+      for (String l in numList){
+        if (i == 3) numFinal += " ";
+        else if (i == 5) numFinal += " ";
+        else if (i == 8) numFinal += " ";
+        i++;
+        numFinal += l;
+      }
+    }
+    
+
+    return numFinal;
+  }
 }
+
